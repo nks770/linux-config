2d6ca54c878f6687452704f09949b7216f983439
Nathaniel K Swenson <nateknute@gmail.com>
2021-03-25 23:23:21 -0500

    Created framework for downloading packages

---
+++ bin/resources.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function downloadPackage() {
+
+  file=""
+  url=""
+
+  while read u ; do
+    if [ ! -z "${u}" ] && [ ! "${u:0:1}" == "#" ] ; then
+      file=${u%%=*}
+      location=${u#*=}
+
+      if [ "${1}" == "${file}" ] ; then
+        url="${location}"
+      fi
+    fi
+  done < ${root}/bin/url.txt
+
+  if [ ! -z "${url}" ] && [ ! -f ${pkg}/${1} ] ; then
+    wget --no-check-certificate "${url}" -O ${pkg}/${1}
+  fi
+
+}
--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ if [ ! -d ${pkg} ] ; then
   mkdir -pv ${pkg}
 fi
 
+source bin/resources.sh
+
+downloadPackage modules-4.7.0.tar.gz
