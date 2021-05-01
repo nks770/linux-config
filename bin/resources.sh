ea6efd4159203fae19f437cdd2fedf8834ce9923
Nathaniel K Swenson <nateknute@gmail.com>
2021-05-01 00:17:07 -0500

    Combine python modules setup into main setup; add a mode to download all packages

--- bin/resources.sh
+++ bin/resources.sh
@@ -26,3 +26,24 @@ function downloadPackage() {
   fi
 
 }
+
+function downloadAllPackages() {
+
+  while read u ; do
+    if [ ! -z "${u}" ] && [ ! "${u:0:1}" == "#" ] ; then
+      file=${u%%=*}
+      url=${u#*=}
+
+      if [ ! -z "${url}" ] && [ ! -f ${pkg}/${file} ] ; then
+        wget --no-check-certificate "${url}" -O ${pkg}/${file}
+      fi
+
+      if [ ! -f ${pkg}/${file} ] ; then
+        echo "ERROR: Could not get ${file}!"
+        exit 1
+      fi
+
+    fi
+  done < ${root}/bin/url.txt
+
+}
--- setup.sh
+++ setup.sh
@@ -35,12 +35,25 @@ if [ "${1}" == "vim" ] ; then
 
 elif [ "${1}" == "python" ] ; then
   check_python 3.9.4
+  check_demjson 3.9.4 2.2.4
+  check_bs4 3.9.4 4.9.3
 
 elif [ "${1}" == "screen" ] ; then
   check_screen 4.8.0
 
 elif [ "${1}" == "ffmpeg" ] ; then
   check_ffmpeg 4.2.2
+
+elif [ "${1}" == "all" ] ; then
+  check_screen 4.8.0
+  check_vim 8.2
+  check_python 3.9.4
+  check_demjson 3.9.4 2.2.4
+  check_bs4 3.9.4 4.9.3
+  check_ffmpeg 4.2.2
+
+elif [ "${1}" == "download" ] ; then
+  downloadAllPackages
 
 else
   check_modules
